e4d58e816e26e2fe9dd0fa2b4afdef12af952c5b
Arpita Verma <arpitaverma8885@gmail.com>
2020-10-02 19:08:11 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,27 @@
+package conditions;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		int dayOfWeek=4;
+		switch(dayOfWeek) {
+		case 1:
+			System.out.println("I am on leave");
+			break;
+		case 2:
+			System.out.println("On work");
+			break;
+		case 3:
+			System.out.println("On work");
+			break;
+		case 4:
+			System.out.println("On work");
+			break;
+		
+		default:
+			System.out.println("I don know");
+		}
+
+	}
+
+}
